ca1b072a8d9deb1b7b188f5ba01644715d51fe49
Edward Opare-Aryee <ed@thisissoon.com>
2014-12-24 19:24:07 +0000

    don't test on FF

--- tests/e2e/protractor.saucelabs.conf.js
+++ tests/e2e/protractor.saucelabs.conf.js
@@ -24,10 +24,10 @@ exports.config = {
     'browserName': 'chrome',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER
-  }, {
-    'browserName': 'firefox',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER
+  // }, {
+  //   'browserName': 'firefox',
+  //   'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+  //   'build': process.env.TRAVIS_BUILD_NUMBER
   }],
   // ----- More information for your tests ----
   //
